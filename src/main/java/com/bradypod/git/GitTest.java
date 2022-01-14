ac577bde59e3838ac4b414e0040a269397f301e6
wzy <1574212572@qq.com>
2022-01-14 16:03:00 +0800

    first commit

---
+++ src/main/java/com/bradypod/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bradypod.git;
+
+/**
+ * @author WuZhenYun
+ * @create 2022-01-14 15:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
